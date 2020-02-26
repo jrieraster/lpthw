ac2943fd5ceb8cc462f3433d25b6b572930b4920
Javi <jrierasterggmail.com>
2020-02-26 00:12:13 -0300

    Salvado desde el Desk_Top

--- ex1.py
+++ ex1.py
@@ -3,6 +3,6 @@ print("Hello Again")
 print("I like typing this.")
 print("This is fun.")
 print('Yay! Printing.')
-print("I'd muvh rather you 'not'.")
+print("I'd much rather you 'not'.")
 print('I"said" do not touch this!')
 
